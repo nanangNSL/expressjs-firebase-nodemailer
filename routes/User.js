cbcae2f3c58b4e319131b1b7c8bec88f0c70ffc6
Nanang Komarudin <45787278+nanangNSL@users.noreply.github.com>
2022-08-14 22:58:27 +0700

    push push

--- routes/User.js
+++ routes/User.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const Users = require("../controllers/User");
-const { upload } = require("../middlewares/multer").default;
+const { upload } = require("../middlewares/multer");
 
 router.route("/:id").patch(Users.Update).get(Users.UsersById)
 router.route("/image/:id").patch(upload("test", "images", "image"), Users.UpdateProfile)
