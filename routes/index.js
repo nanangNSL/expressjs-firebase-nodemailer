514df9db0d2cb2f8199e3233c0cf8f1a042d77b4
Nanang Komarudin <45787278+nanangNSL@users.noreply.github.com>
2022-08-14 23:26:03 +0700

    bismillah

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
 module.exports = (app) => {
   app.use("/auth", require("./Auth"));
   app.use("/users", require("./User"));
-  app.use("/test-multer", require("./multer"));
   app.use("/post", require("./Recipe"))
   app.use("/search", require("./Search.js"))
   app.use("/room", require("./Room"))
