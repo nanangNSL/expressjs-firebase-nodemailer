c2190da1c9a1bf04516abcf851256bf27dc1db1d
Nanang Komarudin <45787278+nanangNSL@users.noreply.github.com>
2022-08-15 12:31:13 +0700

    rm multer midlewares

--- routes/Search.js
+++ routes/Search.js
@@ -1,6 +1,5 @@
 const router = require("express").Router();
 const Recipe = require("../controllers/Recipe");
-const { uploadVideo } = require("../middlewares/multer");
 
 router.route("/").get(Recipe.find)
 router.route("/home").get(Recipe.getAll)
@@ -9,6 +8,6 @@ router.route("/unlike/:id").post(Recipe.Unlike)
 router.route("/comment").post(Recipe.Comment)
 router.route("/save").post(Recipe.SaveRecipe)
 router.route("/save/:id").post(Recipe.Unsave)
-router.route("/video").post(uploadVideo("test", "video", "video"),Recipe.InsertVideo)
+router.route("/video").post(Recipe.InsertVideo)
 
 module.exports = router;
