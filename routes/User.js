8df904ecbb34fbfa35c067fe08e70094c1053f5e
Nanang Komarudin <45787278+nanangNSL@users.noreply.github.com>
2022-08-15 12:25:50 +0700

    test monday again

--- routes/User.js
+++ routes/User.js
@@ -1,8 +1,7 @@
 const router = require("express").Router();
 const Users = require("../controllers/User");
-const { upload } = require("../middlewares/multer.js");
 
 router.route("/:id").patch(Users.Update).get(Users.UsersById)
-router.route("/image/:id").patch(upload("test", "images", "image"), Users.UpdateProfile)
+router.route("/image/:id").patch(Users.UpdateProfile)
 
 module.exports = router;
